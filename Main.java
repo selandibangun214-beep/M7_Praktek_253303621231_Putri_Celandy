38185a8e87368316f24e68610b1052dfa2cfc56f
selandibangun214-beep <selandibangun214@gmail.com>
2026-05-02 10:26:39 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,34 @@
 public class Main {
+
     public static void main(String[] args) {
 
-        StudentRegistration sr = new StudentRegistration();
+        StudentRegistration reg = new StudentRegistration();
 
-        // Uji coba 1: sukses
+        // Uji coba 1 = sukses
         try {
-            sr.register("Putri Celandy", 20);
-
-            // Uji coba 2: nama kosong
-            sr.register("", 22);
-
-            // Uji coba 3: umur tidak valid
-            sr.register("Putri Celandy", 15);
-
+            reg.register("putri celandy", 19);
         } catch (InvalidNameException e) {
-            System.out.println("ERROR NAME: " + e.getMessage());
+            System.out.println("Error Nama: " + e.getMessage());
+        } catch (InvalidAgeException e) {
+            System.out.println("Error Umur: " + e.getMessage());
+        }
+
+        // Uji coba 2 = nama kosong
+        try {
+            reg.register("", 20);
+        } catch (InvalidNameException e) {
+            System.out.println("Error Nama: " + e.getMessage());
+        } catch (InvalidAgeException e) {
+            System.out.println("Error Umur: " + e.getMessage());
+        }
+
+        // Uji coba 3 = umur dibawah 16
+        try {
+            reg.register("Budi", 15);
+        } catch (InvalidNameException e) {
+            System.out.println("Error Nama: " + e.getMessage());
+        } catch (InvalidAgeException e) {
+            System.out.println("Error Umur: " + e.getMessage());
+        }
+    }
+}
